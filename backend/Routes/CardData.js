dd53c8fbe443992f1f65fb4e82fa37d5a249aba2
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-02-11 22:59:14 +0530

    Update CardData.js

--- backend/Routes/CardData.js
+++ backend/Routes/CardData.js
@@ -1,22 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const setCorsHeaders = (res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://foodster-stxh.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-};
-
-router.options("/CardData", (req, res) => {
-  setCorsHeaders(res);
-  res.status(200).end();
-});
-
 router.post("/CardData", (req, res) => {
-  setCorsHeaders(res);
-
   try {
-    res.send([global.food_items, global.food_category]);
+    res.send([global.food_items,global.food_category]);
   } catch (error) {
     console.error(error.message);
     res.send("server error");
@@ -24,17 +11,3 @@ router.post("/CardData", (req, res) => {
 });
 
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-
-// router.post("/CardData", (req, res) => {
-//   try {
-//     res.send([global.food_items,global.food_category]);
-//   } catch (error) {
-//     console.error(error.message);
-//     res.send("server error");
-//   }
-// });
-
-// module.exports = router;
