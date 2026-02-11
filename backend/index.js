edaa3f503a013e666ceb5735fcae6dee89608d71
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-02-11 21:45:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,7 @@ const cors = require('cors');
 
 mongoDB();
 
-app.use(cors({
-  origin: true,
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
-
-app.options("*", cors());
+app.use(cors());
 
 app.use(express.json());
 
@@ -26,30 +19,3 @@ app.get('/', (req, res) => {
 });
 
 module.exports = app;
-
-
-
-
-// const express = require('express');
-// const app = express();
-// const mongoDB = require("./db");
-// const cors = require('cors');
-
-// mongoDB();
-
-// app.use(cors({
-//   origin: process.env.PUBLIC_BASE_URL
-// }));
-
-// app.use(express.json());
-
-// app.use('/api', require("./Routes/CreateUser"));
-// app.use('/api', require("./Routes/Login"));
-// app.use('/api', require("./Routes/CardData"));
-// app.use('/api', require("./Routes/OrderData"));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// module.exports = app;
