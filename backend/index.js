68a61d0f765b90b9e2c32e7f1e3d7ddac722c8ee
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-02-11 21:20:51 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoDB = require("../db");
+const cors = require('cors');
+
+mongoDB();
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
+app.use(express.json());
+
+app.use('/api', require("../Routes/CreateUser"));
+app.use('/api', require("../Routes/Login"));
+app.use('/api', require("../Routes/CardData"));
+app.use('/api', require("../Routes/OrderData"));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
