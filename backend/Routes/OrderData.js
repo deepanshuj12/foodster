066257e0a41d171da170dd3ee93b835dc582313e
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-02-11 22:42:45 +0530

    Update OrderData.js

--- backend/Routes/OrderData.js
+++ backend/Routes/OrderData.js
@@ -1,52 +1,121 @@
 const express = require("express");
 const router = express.Router();
-const Order = require('../models/Orders');
+const Order = require("../models/Orders");
 
-router.post('/orders', async (req, res) => {
-    let data = req.body.order_data;
-
-    data.splice(0, 0, { Order_date: req.body.order_date });
-
-    let eId = await Order.findOne({ 'email': req.body.email });
-
-    if (eId === null) {
-        try {
-            await Order.create({
-                email: req.body.email,
-                order_data: [data] 
-            }).then(() => {
-                res.json({ success: true });
-            });
-        } catch (error) {
-            console.log(error.message);
-            res.status(500).send("server error", error.message);
-        }
-    } else {
-        try {
-
-            let existingOrderData = eId.order_data;
-            existingOrderData.push(data);
-            await Order.updateOne(
-                { email: req.body.email },
-                { $set: { order_data: existingOrderData } }
-            ).then(() => {
-                res.json({ success: true });
-            });
-        } catch (error) {
-            res.status(500).send("server error", error.message);
-        }
-    }
+// Preflight handlers
+router.options("/orders", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodster-stxh.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(200).end();
 });
 
-router.post('/myorders', async (req, res) => {
+router.options("/myorders", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodster-stxh.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(200).end();
+});
+
+router.post("/orders", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodster-stxh.vercel.app");
+
+  let data = req.body.order_data;
+  data.splice(0, 0, { Order_date: req.body.order_date });
+
+  let eId = await Order.findOne({ email: req.body.email });
+
+  if (eId === null) {
     try {
-        let myData = await Order.findOne({ 'email': req.body.email });
-        res.json({ orderData: myData });
+      await Order.create({
+        email: req.body.email,
+        order_data: [data]
+      }).then(() => {
+        res.json({ success: true });
+      });
     } catch (error) {
-        res.status(500).send("server error", error.message);
+      console.log(error.message);
+      res.status(500).send("server error", error.message);
     }
+  } else {
+    try {
+      let existingOrderData = eId.order_data;
+      existingOrderData.push(data);
+      await Order.updateOne(
+        { email: req.body.email },
+        { $set: { order_data: existingOrderData } }
+      ).then(() => {
+        res.json({ success: true });
+      });
+    } catch (error) {
+      res.status(500).send("server error", error.message);
+    }
+  }
+});
+
+router.post("/myorders", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodster-stxh.vercel.app");
+
+  try {
+    let myData = await Order.findOne({ email: req.body.email });
+    res.json({ orderData: myData });
+  } catch (error) {
+    res.status(500).send("server error", error.message);
+  }
 });
 
 module.exports = router;
+
+
+// const express = require("express");
+// const router = express.Router();
+// const Order = require('../models/Orders');
+
+// router.post('/orders', async (req, res) => {
+//     let data = req.body.order_data;
+
+//     data.splice(0, 0, { Order_date: req.body.order_date });
+
+//     let eId = await Order.findOne({ 'email': req.body.email });
+
+//     if (eId === null) {
+//         try {
+//             await Order.create({
+//                 email: req.body.email,
+//                 order_data: [data] 
+//             }).then(() => {
+//                 res.json({ success: true });
+//             });
+//         } catch (error) {
+//             console.log(error.message);
+//             res.status(500).send("server error", error.message);
+//         }
+//     } else {
+//         try {
+
+//             let existingOrderData = eId.order_data;
+//             existingOrderData.push(data);
+//             await Order.updateOne(
+//                 { email: req.body.email },
+//                 { $set: { order_data: existingOrderData } }
+//             ).then(() => {
+//                 res.json({ success: true });
+//             });
+//         } catch (error) {
+//             res.status(500).send("server error", error.message);
+//         }
+//     }
+// });
+
+// router.post('/myorders', async (req, res) => {
+//     try {
+//         let myData = await Order.findOne({ 'email': req.body.email });
+//         res.json({ orderData: myData });
+//     } catch (error) {
+//         res.status(500).send("server error", error.message);
+//     }
+// });
+
+// module.exports = router;
 
 
